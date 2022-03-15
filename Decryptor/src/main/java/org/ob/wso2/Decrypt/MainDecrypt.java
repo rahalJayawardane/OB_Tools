a76c0a80b7391a2fbc72421b7a18a4ec37bc3164
rahaljaya2012@gmail.com <rahaljaya2012@gmail.com>
2022-03-15 18:04:58 +0530

    decode encrypt value before decrypt

--- Decryptor/src/main/java/org/ob/wso2/Decrypt/MainDecrypt.java
+++ Decryptor/src/main/java/org/ob/wso2/Decrypt/MainDecrypt.java
@@ -1,6 +1,7 @@
 package org.ob.wso2.Decrypt;
 
 
+
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import java.io.StringWriter;
@@ -35,47 +36,14 @@ public class MainDecrypt {
         String file = "/Users/rahal/Desktop/ARB_Docker/wso2-obam-1.3.0/repository/resources/security/wso2carbon.jks"; // args[1];
         String encryptedTextKey = "eyJjIjoiZmhpSFF6VnNYTy8vMjVySzgrdXhaQllqY2htSVgzWkRZbmhpRFh0UlllUmo5Y1J4MnNGN3BIZC82dGt3c0RGd0p0ZXVGK1BPL3ExYVNMN1ZtZTVPRExGNmdZV1k4NW16Wmp3RGFUOVc2U0VOa2RVUEJtblMrN3llQVMweFVBaWNTNSsrcmtlcXY2b2tkN3d1bFRWS2dsYklacy9RVDBLNHJQMHhFY1cwTWhiU2FQTWlXc3o2VzRuYmJ5NUllT0h0bklxZlJyVDV6MGl0NmdZQXpZSWRsS0ZnbmJhRWhLRkNzdHBORXB2RUpjWnBBQkhwVjFnL0NBNHoyQXhTWmMwSCs2elZpMG9vV0VTZEpJZDlJVC9mN20ybnJmUVBqRC9JbDRwZjl0ZGlzL2trZWJzN2xGZnJQOUNhaHlBMW9oRjNVWVJVVGE4alROTmc2cWxjOGhUNm13XHUwMDNkXHUwMDNkIiwidCI6IlJTQS9FQ0IvT0FFUHdpdGhTSEExYW5kTUdGMVBhZGRpbmciLCJ0cCI6IjUwMUZDMTQzMkQ4NzE1NURDNDMxMzgyQUVCODQzRUQ1NThBRDYxQjEiLCJ0cGQiOiJTSEEtMSJ9"; //  args[2];
         getKeysFromKeyStore(file, password, password, password);
-//        String enc = encryptPlainText("arbUK@123");
-        String string = "arbUK@123";
-//        byte[] ciper = encrypt(privateKey, string.getBytes(StandardCharsets.UTF_8));
-//        System.out.println(ciper.toString());
-//        System.out.println(decrypt(publicKey, ciper));
+        byte[] decodeCipher = Base64.getDecoder().decode(encryptedTextKey.getBytes(StandardCharsets.UTF_8));
+        JSONObject json = new JSONObject(new String(decodeCipher));
+        String cipher = json.getString("c");
 
-        byte[] ciper = generateEncryptedData(string);
-        String string1 = new String(ciper);
-        System.out.println(string1);
-
-        System.out.println(generateDecryptedData("fhiHQzVsXO//25rK8+uxZBYjchmIX3ZDYnhiDXtRYeRj9cRx2sF7pHd/6tkwsDFwJteuF+PO/q1aSL7Vme5ODLF6gYWY85mzZjwDaT9W6SENkdUPBmnS+7yeAS0xUAicS5++rkeqv6okd7wulTVKglbIZs/QT0K4rP0xEcW0MhbSaPMiWsz6W4nbby5IeOHtnIqfRrT5z0it6gYAzYIdlKFgnbaEhKFCstpNEpvEJcZpABHpV1g/CA4z2AxSZc0H+6zVi0ooWESdJId9IT/f7m2nrfQPjD/Il4pf9tdis/kkebs7lFfrP9CahyA1ohF3UYRUTa8jTNNg6qlc8hT6mw==".getBytes(StandardCharsets.UTF_8)));
-
-//        decrypt(password, file, enc);
-//        eyJjIjoiZmhpSFF6VnNYTy8vMjVySzgrdXhaQllqY2htSVgzWkRZbmhpRFh0UlllUmo5Y1J4MnNGN3BIZC82dGt3c0RGd0p0ZXVGK1BPL3ExYVNMN1ZtZTVPRExGNmdZV1k4NW16Wmp3RGFUOVc2U0VOa2RVUEJtblMrN3llQVMweFVBaWNTNSsrcmtlcXY2b2tkN3d1bFRWS2dsYklacy9RVDBLNHJQMHhFY1cwTWhiU2FQTWlXc3o2VzRuYmJ5NUllT0h0bklxZlJyVDV6MGl0NmdZQXpZSWRsS0ZnbmJhRWhLRkNzdHBORXB2RUpjWnBBQkhwVjFnL0NBNHoyQXhTWmMwSCs2elZpMG9vV0VTZEpJZDlJVC9mN20ybnJmUVBqRC9JbDRwZjl0ZGlzL2trZWJzN2xGZnJQOUNhaHlBMW9oRjNVWVJVVGE4alROTmc2cWxjOGhUNm13XHUwMDNkXHUwMDNkIiwidCI6IlJTQS9FQ0IvT0FFUHdpdGhTSEExYW5kTUdGMVBhZGRpbmciLCJ0cCI6IjUwMUZDMTQzMkQ4NzE1NURDNDMxMzgyQUVCODQzRUQ1NThBRDYxQjEiLCJ0cGQiOiJTSEEtMSJ9
-//        Hj6Aa4Po8K+8DHpUvDtXbDCDZZR+HtnD2dWqUzZufxnehQKScUn9B3UiQBT6euvmjl/pQax7+gB0kgYtxIfB3XQtEfn+ZZeS9JbFHQ/pbTpOen9vxLSEBFistR6hJBJugXeSJZxDP+vbj1KP2j/vOvx6AAtLhgOpevADJGm7QKvwKBQNGhR6ozzHJPa43LbtoBpDzscFt5C19eyVjEMuNX2pTVINxK5Khw0SVvbNAilr69a/CCpshoI8rd+ewOn7x9mrO3TQ8MLgKa2IY1FdXrXMW4ieDWI3FmkAz470aqPFMWZav2boxM3TVKbi5CeNNlfQRPtAqnPT7lu500Uveg==
-        //fhiHQzVsXO//25rK8+uxZBYjchmIX3ZDYnhiDXtRYeRj9cRx2sF7pHd/6tkwsDFwJteuF+PO/q1aSL7Vme5ODLF6gYWY85mzZjwDaT9W6SENkdUPBmnS+7yeAS0xUAicS5++rkeqv6okd7wulTVKglbIZs/QT0K4rP0xEcW0MhbSaPMiWsz6W4nbby5IeOHtnIqfRrT5z0it6gYAzYIdlKFgnbaEhKFCstpNEpvEJcZpABHpV1g/CA4z2AxSZc0H+6zVi0ooWESdJId9IT/f7m2nrfQPjD/Il4pf9tdis/kkebs7lFfrP9CahyA1ohF3UYRUTa8jTNNg6qlc8hT6mw==
+        String generateDecryptedData = generateDecryptedData(cipher.getBytes(StandardCharsets.UTF_8));
+        System.out.println(generateDecryptedData);
     }
 
-    public static void decrypt(String password, String file, String encryptedTextKey) {
-        byte[] decryptedData = null;
-
-        try {
-            FileInputStream is = new FileInputStream(file);
-            KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
-            keystore.load(is, password.toCharArray());
-            String alias = password;
-            Key key = keystore.getKey(alias, password.toCharArray());
-
-            Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPwithSHA1andMGF1Padding","BC");
-            cipher.init(Cipher.DECRYPT_MODE, key);
-            decryptedData = cipher.doFinal(encryptedTextKey.getBytes());
-            System.out.println("Decrypted Key: " + decryptedData);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-//
-//
-
-//
     private static PrivateKey getKeysFromKeyStore(String keyStoreFilePath, String keyStorePassword,
                                                  String privateKeyCertAlias, String privateKeyPassword) throws Exception {
         try {
